52f674e3fe134ba585c7f89cc6fe54026592b4af
Jeff Man <jeffman231@hotmail.com>
2026-03-09 20:11:11 +0000

    feat: update nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  output: 'standalone',
+  outputFileTracingRoot: __dirname,
 };
 
 export default nextConfig;
